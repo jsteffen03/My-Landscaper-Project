218761ee42c2ea5a9b2b386a75fade3a145c6fd7
Justin Steffen <jcsteffen03@gmail.com>
2024-09-12 16:22:12 -0600

    Final Commit

--- client/src/Components/LoginLandscaper.tsx
+++ client/src/Components/LoginLandscaper.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom'  
 import { FormField, Button, Form } from 'semantic-ui-react'
-import { User, Plant, Landscaper } from '../types';
+import { Landscaper } from '../types';
 
 type NewLandscaper = {
     name: string;
